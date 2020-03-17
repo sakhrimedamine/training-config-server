a2ef9c5fc45bb6001515d4b1ab19e98105f9b9a9
sakhrimedamine <sakhrimedamine@gmail.com>
2020-03-17 14:57:28 +0100

    create project

---
+++ src/test/java/com/sakhri/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sakhri.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
